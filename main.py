6c19f0868d629c9afa9cd6752984fd1a735dc654
takashiAg <crawd4274@gmail.com>
2018-11-01 06:56:50 +0900

    エラーハンドラを追加した

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import pyaudio
 import threading
-import numpy as np
+from numpy import frombuffer, array
 import time
 
-CHUNK = 1024
+CHUNK = 64
 RATE = 44100
 p = pyaudio.PyAudio()
 
@@ -12,7 +12,7 @@ input_value = [0 for x in range(1024)]
 
 
 class ThreadJob(threading.Thread):
-    def __init__(self, v=[]):
+    def __init__(self, v=""):
         threading.Thread.__init__(self)
         self.line = v
         self.kill_flag = False
@@ -20,9 +20,11 @@ class ThreadJob(threading.Thread):
     def run(self):
         old = []
         while not (self.kill_flag):
-            plt.plot(self.line[:])
-            plt.pause(0.001)
-            print(self.line)
+            if (self.line and self.line != old):
+                plt.plot(self.line[:])
+                plt.pause(0.001)
+                print(self.line)
+                old = self.line
 
 
 def main():
@@ -30,13 +32,12 @@ def main():
     t.start()
     try:
         stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, frames_per_buffer=CHUNK, input=True,
-                        output=False)  # inputとoutputを同時にTrueにする
+                        output=True)  # inputとoutputを同時にTrueにする
         while stream.is_active():
             input = stream.read(CHUNK)
+            t.line=[x for x in frombuffer(input, dtype="int16")]
 
-            data = [x for x in np.frombuffer(input, dtype="int16")]
-            t.line = np.append(t.line, data)
-            # output = stream.write(input)
+            output = stream.write(input)
 
         stream.stop_stream()
         stream.close()

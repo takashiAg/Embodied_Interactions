b64e1573571302a3e43101d0606370236222ece7
takashiAg <crawd4274@gmail.com>
2018-11-08 17:12:26 +0900

    送る形へ整形した

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Team Member :
 # グラフ表示したいときはこれをTrue
 graph = False
 # 処理時間を計測したいときはこれをTrue
-process_time_mesurement = True
+process_time_mesurement = False
 # カラーを表示するとき
 color_display = False
 
@@ -92,6 +92,7 @@ p2.start()
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import colorsys
 
 # %matplotlib inline
 
@@ -112,6 +113,9 @@ data = d["data"][:] / 32768.0
 fig1_a_1, = fig1_a.plot(data)
 fig1_b_1, = fig1_b.plot(data)
 
+h_filtered = 0.0
+h_filtered2 = 0.0
+old_pgm = 0
 start = time.time()
 while True:
     start = time.time()
@@ -127,7 +131,7 @@ while True:
     fft_data[0]   の方が低い音
     fft_data[100] の方が高い音
     '''
-    fft_data = np.fft.fft(data).real[:int(len(x) / 2)][:100]
+    fft_data = np.fft.fft(data).real[:int(len(x) / 2)][:128]
     x_axis_fft = list(range(len(fft_data)))
 
     if color_display:
@@ -140,10 +144,6 @@ while True:
         '''
         print(color)
 
-    # ↓↓↓↓↓↓↓↓この辺にコード書いて！↓↓↓↓↓↓↓↓↓↓
-
-    # ↑↑↑↑↑↑↑↑この辺にコード書いて！↑↑↑↑↑↑↑↑↑↑
-
     if graph:
         fig1_a_1.set_data(x, data)
         fig1_a.set_ylim(-1.3, 1.3)
@@ -153,6 +153,23 @@ while True:
         fig1_b.set_xlim(min(x_axis_fft), max(x_axis_fft))
 
         plt.pause(0.1)
+
+    # ↓↓↓↓↓↓↓↓この辺にコード書いて！↓↓↓↓↓↓↓↓↓↓
+    max_freq_value = fft_data.max()
+    max_freq_index = fft_data.argmax()
+    h, s, v = colorsys.rgb_to_hsv(int(color["r"]), int(color["g"]), int(color["b"]))
+    # h=float(h)
+    per = 0.001
+    h_filtered = per * h + (1.0 - per) * h_filtered
+    h_filtered2 = per * h_filtered + (1.0 - per) * h_filtered2
+    pgm = int(h_filtered2 * 128 - 1)
+    if abs(old_pgm - pgm) < 2:
+        pgm = old_pgm
+    else:
+        old_pgm = pgm
+
+    print("\r", max_freq_value, '\t', max_freq_index, '\t', pgm, '\t', h, '\t', h_filtered, '\t', h_filtered2, end="")
+    # ↑↑↑↑↑↑↑↑この辺にコード書いて！↑↑↑↑↑↑↑↑↑↑
 
     # 処理にかかった時間
     if process_time_mesurement:
